6aace410437f01d120b70d8a1cdf133537032d05
andreluiz <118825154+andrepipino@users.noreply.github.com>
2024-06-06 09:51:57 -0300

    06/06/24

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const botoes=document.querySelectorAll(".botao");
+const conteudo  = document.querySelectorAll(aba-conteudo)
+
 console.log(botoes)
 for (let  i = 0 ; i < botoes.length ; i++){
     botoes[i].onclick = function () {
         for (let j = 0; j < botoes.length; j++){
             botoes[j].classList.remove("ativo")
+            abaconteudo[j].classList.remove("ativo")
         }
         botoes[i].classList.add("ativo")
+        abaconteudo[j].classList.remove("ativo")
+
     }
 }
